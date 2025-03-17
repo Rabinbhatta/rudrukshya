da751923e50736664a65e3300294f6668e2908a5
Nischal Neupane <102789731+Nischal1111@users.noreply.github.com>
2025-03-17 21:54:17 +0545

    UI-2

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,10 +1,11 @@
 import Products from '@/component/products'
+import { josefin } from '@/utils/font'
 import React from 'react'
 
 const product = () => {
   return (
     <div>
-            <h1 className='text-4xl'>Products list</h1>
+            <h1 className={`text-4xl text-primaryColor ${josefin.className}`}>Products list</h1>
             <div><Products/></div>
             </div>
   )
